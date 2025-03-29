a5a3e06b135878243f7019256b5acbc60a9c1c20
Florian <Florian@MacBook-Pro.local>
2025-03-29 07:02:48 +0100

    route-buy-ajoutée

--- src/api/offer/controllers/offer.js
+++ src/api/offer/controllers/offer.js
@@ -4,6 +4,7 @@
  * offer controller
  */
 
+const stripe = require('stripe')(process.env.STRIPE_API_SECRET);
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::offer.offer', ({ strapi }) => ({
@@ -15,33 +16,39 @@ module.exports = createCoreController('api::offer.offer', ({ strapi }) => ({
         const requestBody = ctx.state.user
         const userId = requestBody.id
         
-
         // utiliser l'Entity Service Api de Strapi pour récupérer les informations de cet utilisateur et, notamment, les références vers les offres qu'il a postées ; vous allez avoir besoin de déployer (populate) la clef offers de l'utilisateur
         
         const user = await strapi.entityService.findOne("plugin::users-permissions.user", userId, {populate: "offers"})
 
-       
-
-       
-
-        
         // parcourir le tableau de références avec une boucle et supprimer chacune d'entre elles grâce à l'Entity Service API de Strapi
         for (let i=0; i<user.offers.length; i++) {
          let offersId = user.offers[i].id
           await strapi.entityService.delete("api::offer.offer", offersId)
         }
-
-
-
-
         return "Toutes les offres ont bien été supprimées"
-
         // répondre au client
         
-
       } catch (error) {
         ctx.response.status = 500;
         return { message: error.message };
       }
     },
-  }));
+    async buy(ctx) {
+      try {
+        const { status } = await stripe.charges.create({
+         
+          amount: ctx.request.body.amount * 100,
+          currency: "eur",
+          description: `Paiement le bon coin : ${ctx.request.body.title}`,
+          source: ctx.request.body.token,
+        });
+  
+        return { status: status };
+      } catch (error) {
+        ctx.response.status = 500;
+        return { message: error.message };
+      }
+    },
+  }))
+  
+ 
